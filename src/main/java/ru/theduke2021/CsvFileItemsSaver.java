3fe881fa2d06e2ee28fab0603de0a8680ef94bdd
TheDuke2021 <rbk_74@mail.ru>
2024-03-13 18:10:28 +0300

    interface for saving results in CSV

---
+++ src/main/java/ru/theduke2021/CsvFileItemsSaver.java
@@ -0,0 +1,15 @@
+package ru.theduke2021;
+
+import java.util.List;
+
+public class CsvFileItemsSaver implements ItemsSaver {
+
+    private final String FILENAME = "regard-items.csv";
+
+
+    @Override
+    public void saveItems(List<Item> items) {
+
+    }
+
+}
---
+++ src/main/java/ru/theduke2021/ItemsSaver.java
@@ -0,0 +1,9 @@
+package ru.theduke2021;
+
+import java.util.List;
+
+public interface ItemsSaver {
+
+    public void saveItems(List<Item> items);
+
+}
